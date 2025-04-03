626383515431ed9a299e6d7bc87e98b3e6c69081
Regulus128 <nguyenletam1282006@gmail.com>
2025-04-04 00:39:11 +0800

    fix bugs

--- model/Report.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Report {
-
-    // Placeholder method — actual filters will be handled later
-    public String generate() {
-        return "===== REPORT GENERATED (placeholder) =====";
-    }
-}
---
+++ src/controller/UserController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import model.Applicant;
+import model.HDBManager;
+import model.HDBOfficer;
+import model.User; 
+import repository.UserRepository;
+
+
+public class UserController {
+    private UserRepository userRepository;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+    public User login(String nric, String password) {
+        for (Applicant a : userRepository.getApplicants()) {
+            if(a.getNric().equals(nric) && a.getPassword().equals(password))
+                return a;
+        }
+        for (HDBOfficer o : userRepository.getOfficers()) {
+            if(o.getNric().equals(nric) && o.getPassword().equals(password))
+                return o;
+        }
+        for (HDBManager m : userRepository.getManagers()) {
+            if(m.getNric().equals(nric) && m.getPassword().equals(password))
+                return m;
+        }
+        return null;
+    }
+    
+    public boolean changePassword(User user, String newPassword) {
+        user.setPassword(newPassword);
+        // After password change, update the CSV file based on the user type.
+        if(user instanceof HDBManager) {
+            userRepository.saveManagers();
+        } else if(user instanceof HDBOfficer) {
+            userRepository.saveOfficers();
+        } else if(user instanceof Applicant) {
+            userRepository.saveApplicants();
+        }
+        return true;
+    }
+}
